62cd52a4411f1864e18982766c7570102a7d2f6a
wwwwwalter <15221024927@163.com>
2019-05-27 23:25:07 +0800

    script

--- linux/script1a.sh
+++ linux/script1a.sh
@@ -3,4 +3,6 @@
 #Author: walter
 #Date: Mon May 27 02:05:09 CST 2019
 
+awk '{printf $2 "\t" $6 "\n"}' file
 
+df -h | awk '{print $1 "\t" $3}'
--- linux/script1b.sh
+++ linux/script1b.sh
@@ -3,4 +3,18 @@
 #Author: walter
 #Date: Mon May 27 02:05:09 CST 2019
 
+awk 'BEGIN{printf"this is a table\n"}
+{print $2 "\t" $6}
+END{print"THE END"}' file
 
+echo "---"
+
+cat file | grep -v Name | awk '$6>=87{print $0}'
+
+echo "---"
+
+awk '/Gao/ {print}' file
+
+echo "---"
+
+df -h | awk '/sda[0-9]/{print}'
