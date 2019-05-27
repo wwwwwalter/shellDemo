a05be30a1bd13b8fac1223f881419e34a317f530
wwwwwalter <15221024927@163.com>
2019-05-28 02:29:39 +0800

    script

--- linux/script2b.sh
+++ linux/script2b.sh
@@ -3,4 +3,14 @@
 #Author: walter
 #Date: Mon May 27 02:05:09 CST 2019
 
+echo -e "$(df -h)\n"
 
+rate=$(df -h |
+    grep "rootfs" |
+    awk '{print $5}' |
+    cut -d "%" -f1)
+echo ${rate}
+
+if [ ${rate} -ge 40 ]; then
+    echo "warning"
+fi
